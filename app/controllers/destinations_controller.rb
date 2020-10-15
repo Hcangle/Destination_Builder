5c95c9d1f95fdd5bd3b508b98808b0178c64843f
Hangle <hannahcangle@gmail.com>
2020-10-15 17:38:38 -0500

    Controller and migrations created

--- app/controllers/destinations_controller.rb
+++ app/controllers/destinations_controller.rb
@@ -2,5 +2,6 @@ class DestinationsController < ApplicationController
 
     def index 
         @destinations = Destination.all 
+        #im just adding something new so this actually saves 
     end 
 end 
