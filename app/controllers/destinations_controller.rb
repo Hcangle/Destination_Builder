d218c5d2c0449c0c8dd310d65d658bb4d104440b
Hangle <hannahcangle@gmail.com>
2020-10-18 13:28:46 -0500

    Views set up

--- app/controllers/destinations_controller.rb
+++ app/controllers/destinations_controller.rb
@@ -1,7 +1,25 @@
 class DestinationsController < ApplicationController
 
     def index 
-        @destinations = Destination.all 
-        
+        @destinations = Destination.all  
+    end 
+
+    def new
+        @destination = Destination.new   
+    end 
+
+    def create 
+       @destination = Destination.new(destination_params)
+       if @destination.save
+         redirect_to destinations_path(@destination)
+       else 
+        render :new
+       end 
+    end 
+
+    private 
+
+    def destination_params
+       params.require(:destination).permit(:location)
     end 
 end 
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-resources :destinations, only: :index
+resources :destinations
 resources :routes, only: [:index, :create, :new, :show, :edit] do 
   resources :directions 
   
